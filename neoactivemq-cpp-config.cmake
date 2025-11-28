cbbe20c01f6f7021c89d6fee1c65f0f458bca360
Prachya Saechua <psaechua@cpaxtra.co.th>
2025-11-28 11:27:59 +0700

    Rename project to neoactivemq-cpp and update CMake configuration for namespaced usage
    Signed-off-by: Prachya Saechua <psaechua@cpaxtra.co.th>

---
+++ neoactivemq-cpp-config.cmake
@@ -0,0 +1,20 @@
+# neoactivemq-cpp-config.cmake
+# CMake configuration file for neoactivemq-cpp library
+# This file allows find_package(neoactivemq-cpp) to work in consuming projects
+
+@PACKAGE_INIT@
+
+include(CMakeFindDependencyMacro)
+
+# Find required dependencies
+find_dependency(apr CONFIG)
+
+# Optional: OpenSSL support
+if(@AMQCPP_HAVE_OPENSSL@)
+    find_dependency(OpenSSL)
+endif()
+
+# Include the targets file
+include("${CMAKE_CURRENT_LIST_DIR}/neoactivemq-cpp-targets.cmake")
+
+check_required_components(neoactivemq-cpp)
